3384402f0466ee6aee6ee0cd615c44be55f08d57
Waverunner <lewaverunner@gmail.com>
2017-03-19 20:17:06 -0400

    Update to use flex-mark for markdown

--- src/com/waverunnah/swg/harvesterdroid/gui/dialog/AboutDialog.java
+++ src/com/waverunnah/swg/harvesterdroid/gui/dialog/AboutDialog.java
@@ -1,35 +1,54 @@
 package com.waverunnah.swg.harvesterdroid.gui.dialog;
 
 
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.parser.ParserEmulationProfile;
+import com.vladsch.flexmark.util.options.MutableDataSet;
 import javafx.scene.Node;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.HTMLEditor;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
-import org.pegdown.PegDownProcessor;
 
-import java.io.CharArrayReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.nio.file.Files;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class AboutDialog extends Dialog {
+
+	private HTMLEditor htmlView;
 
 	public AboutDialog() {
 		super();
 		try {
 			init();
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void init() throws FileNotFoundException {
+	private void init() throws IOException {
 		setTitle("About");
+		htmlView = createHtmlView();
+
+		renderReadme();
+
+		getDialogPane().setContent(new AnchorPane(htmlView));
+		getDialogPane().getButtonTypes().add(ButtonType.OK);
+	}
+
+	private void renderReadme() throws IOException {
+		MutableDataSet options = new MutableDataSet();
+		options.setFrom(ParserEmulationProfile.GITHUB_DOC);
+
+		Parser parser = Parser.builder(options).build();
+		HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+		com.vladsch.flexmark.ast.Node document = parser.parseReader(new FileReader("README.md"));
+		htmlView.setHtmlText(renderer.render(document));
+	}
+
+	private HTMLEditor createHtmlView() {
 		HTMLEditor htmlEditor = new HTMLEditor();
 
 		Node[] nodes = htmlEditor.lookupAll(".tool-bar").toArray(new Node[0]);
@@ -43,21 +62,6 @@ public class AboutDialog extends Dialog {
 		AnchorPane.setLeftAnchor(htmlEditor, 0d);
 		AnchorPane.setTopAnchor(htmlEditor, 0d);
 		AnchorPane.setBottomAnchor(htmlEditor, 0d);
-
-		PegDownProcessor processor = new PegDownProcessor();
-
-		StringBuilder builder = new StringBuilder();
-
-		File file = new File("README.md");
-		Scanner scanner = new Scanner(file);
-
-		builder.append(scanner.nextLine());
-		while (scanner.hasNextLine())
-			builder.append("\n").append(scanner.nextLine());
-
-		htmlEditor.setHtmlText(processor.markdownToHtml(builder.toString()));
-
-		getDialogPane().setContent(new AnchorPane(htmlEditor));
-		getDialogPane().getButtonTypes().add(ButtonType.OK);
+		return htmlEditor;
 	}
 }
