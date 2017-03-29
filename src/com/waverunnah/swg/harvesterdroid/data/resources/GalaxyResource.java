02fc78346a1b0294e45053c236a4a01e0ec9c8ef
Waverunner <lewaverunner@gmail.com>
2017-03-29 01:51:50 -0400

    Added GNU GPLv3 License

--- src/com/waverunnah/swg/harvesterdroid/data/resources/GalaxyResource.java
+++ src/com/waverunnah/swg/harvesterdroid/data/resources/GalaxyResource.java
@@ -1,12 +1,37 @@
+/*
+ * HarvesterDroid - A Resource Tracker for Star Wars Galaxies
+ * Copyright (C) 2017  Waverunner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.waverunnah.swg.harvesterdroid.data.resources;
 
 import com.waverunnah.swg.harvesterdroid.utils.Attributes;
-import javafx.beans.property.*;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GalaxyResource {
     private StringProperty name = new SimpleStringProperty();
--- src/com/waverunnah/swg/harvesterdroid/xml/app/SchematicsXml.java
+++ src/com/waverunnah/swg/harvesterdroid/xml/app/SchematicsXml.java
@@ -1,14 +1,33 @@
+/*
+ * HarvesterDroid - A Resource Tracker for Star Wars Galaxies
+ * Copyright (C) 2017  Waverunner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.waverunnah.swg.harvesterdroid.xml.app;
 
 import com.waverunnah.swg.harvesterdroid.data.schematics.Schematic;
 import com.waverunnah.swg.harvesterdroid.xml.BaseXml;
-
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
