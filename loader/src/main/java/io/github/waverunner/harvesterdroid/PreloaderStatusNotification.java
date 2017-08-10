4f4908f2fd8b9dba26308466399d979083583b3d
Waverunner <lewaverunner@gmail.com>
2017-08-10 14:58:00 -0400

    Changed project structure to a multi-project style

--- loader/src/main/java/io/github/waverunner/harvesterdroid/PreloaderStatusNotification.java
+++ loader/src/main/java/io/github/waverunner/harvesterdroid/PreloaderStatusNotification.java
@@ -21,16 +21,16 @@ package io.github.waverunner.harvesterdroid;
 import javafx.application.Preloader;
 
 public class PreloaderStatusNotification extends Preloader.ProgressNotification {
-	private String status;
+  private String status;
 
-	public PreloaderStatusNotification(String status, double value) {
-		super(value);
-		this.status = status;
-	}
+  public PreloaderStatusNotification(String status, double value) {
+    super(value);
+    this.status = status;
+  }
 
-	public String getStatus() {
-		return status;
-	}
+  public String getStatus() {
+    return status;
+  }
 
 
 }
