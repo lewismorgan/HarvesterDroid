d2376c37da2e2a5f08d97b5b49d1b742196ec532
Waverunner <lewaverunner@gmail.com>
2017-11-12 01:44:32 -0500

    Java 9 compiling for api, loader, and tracker:gh

---
+++ trackers/gh/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lewis on 11/12/17
+ */
+module io.github.waverunner.harvesterdroid.trackers.gh {
+  requires io.github.waverunner.harvesterdroid.api;
+  requires java.xml;
+  requires java.xml.bind;
+}
