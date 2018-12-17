1617108c0151a5b2c080dd964f48d7a0cc38d77e
Lewis <lewismorgan1995@icloud.com>
2018-12-16 19:46:21 -0500

    Use semver gradle plugin for versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,29 @@
+import java.io.ByteArrayOutputStream
+import net.thauvin.erik.gradle.semver.SemverIncrementBuildMetaTask
+import net.thauvin.erik.gradle.semver.SemverIncrementTask
+
 plugins {
   base
   kotlin("jvm") version "1.3.11"
   java
   checkstyle
+  id("net.thauvin.erik.gradle.semver") version "0.9.8-beta"
 }
 
 val spekVersion = "2.0.0-rc.1"
 val junitVersion = "5.3.2"
 
 allprojects {
-  group = "com.lewisjmorgan.harvesterdroid"
-  version = "2.0.0-SNAPSHOT"
-
   apply(plugin = "java")
   apply(plugin = "org.jetbrains.kotlin.jvm")
+  apply(plugin = "net.thauvin.erik.gradle.semver")
+
+  java {
+    this.sourceCompatibility = JavaVersion.VERSION_1_8
+    this.sourceCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  group = "com.lewisjmorgan.harvesterdroid"
 
   repositories {
     jcenter()
@@ -27,36 +37,43 @@ allprojects {
     compile("io.reactivex.rxjava2:rxjava:2.2.4")
     compile("io.reactivex.rxjava2:rxkotlin:2.3.0")
 
-    addTestDependencies(this)
+    addHarvesterDroidTestDependencies(this)
   }
 
   java.sourceCompatibility = JavaVersion.VERSION_1_8
   java.targetCompatibility = JavaVersion.VERSION_1_8
 
-  tasks.withType<Jar> {
-    baseName = "harvesterdroid"
-    appendix = project.name
-  }
+  tasks {
+    val incrementBuildMetaTask = withType<SemverIncrementBuildMetaTask> {
+      //println("Current build metadata: ${this.buildMeta}")
+      doFirst {
+        buildMeta = acquireCurrentGitCommitHash(true)
+      }
+    }
 
-  tasks.withType<Checkstyle> {
-    configFile = File(rootDir, "checkstyle.xml")
-  }
+    withType<Jar> {
+      baseName = "harvesterdroid"
+      appendix = project.name
+      // Only want the jar's to increment build meta since that's what the user will be running.
+      dependsOn(incrementBuildMetaTask)
+      doLast {
+        println("VERSION: ${project.version}")
+      }
+    }
 
-  tasks.withType<Test> {
-    useJUnitPlatform {
-      includeEngines("spek2")
+    withType<Test> {
+      maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+      useJUnitPlatform {
+        includeEngines("spek2")
+      }
+    }
+    withType<Checkstyle> {
+      configFile = File(rootDir, "checkstyle.xml")
     }
   }
 }
 
-dependencies {
-  // Make the root project archives configuration depend on every sub project
-  subprojects.forEach {
-    archives(it)
-  }
-}
-
-fun addTestDependencies(scope: DependencyHandlerScope) {
+fun addHarvesterDroidTestDependencies(scope: DependencyHandlerScope) {
   scope {
     // Testing Dependencies
     testImplementation(kotlin("reflect"))
@@ -77,4 +94,13 @@ fun addTestDependencies(scope: DependencyHandlerScope) {
 
     testRuntime("org.junit.platform:junit-platform-launcher:1.3.2")
   }
+}
+
+fun acquireCurrentGitCommitHash(short: Boolean): String {
+  val stdout = ByteArrayOutputStream()
+  exec {
+    commandLine("git", "rev-parse", if (short) "--short" else null, "HEAD")
+    standardOutput = stdout
+  }
+  return stdout.toString().trim()
 }
